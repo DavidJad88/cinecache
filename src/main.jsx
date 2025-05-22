cfd3256fcb2ea19511bf0603522c21fd67ff2ff8
DavidJad88 <davidjad88@gmail.com>
2025-05-22 09:43:00 +0200

    fix error with route import in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import App from "./App.jsx";
-import { router } from "./routes/routes.jsx";
+import { router } from "./routes/Routes.jsx";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/authContext.jsx";
 

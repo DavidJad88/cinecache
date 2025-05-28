f4b84533247cd11d911b5395ac1bfd7b57ba20f1
DavidJad88 <davidjad88@gmail.com>
2025-05-28 10:45:10 +0200

    implement contact form

---
+++ src/hooks/useContactValidation.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const useContactValidation = () => {
+  const [contactErrors, setContactErrors] = useState({});
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const validateContactForm = (values) => {
+    let newErrors = {};
+
+    if (!values.firstName.trim()) {
+      newErrors.firstName = "First name is required";
+    }
+    if (!values.lastName.trim()) {
+      newErrors.lastName = "Last name is required";
+    }
+    if (!values.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!emailRegex.test(values.email)) {
+      newErrors.email = "Please enter a valid email address";
+    }
+    if (!values.subject.trim()) {
+      newErrors.subject = "Subject is required";
+    }
+    if (!values.message.trim()) {
+      newErrors.message = "Message is required";
+    }
+    setContactErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+  const validateMessageLength = (value, maxLength) => {
+    setContactErrors((prevErrors) => ({
+      ...prevErrors,
+      message:
+        value.trim().length >= maxLength
+          ? `Maximum characters allowed is ${maxLength}`
+          : "",
+    }));
+  };
+
+  return {
+    contactErrors,
+    validateContactForm,
+    validateMessageLength,
+  };
+};
+
+export default useContactValidation;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,226 @@
 import styles from "./Contact.module.css";
 
+import Button from "../../components/Button/Button";
+import Modal from "../../components/Modal/Modal";
+
+import { getAuthContext } from "../../context/authContext";
+import { database } from "../../../firebaseConfig";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+
+import useContactValidation from "../../hooks/useContactValidation";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [currentUserData, setCurrentUserData] = useState({});
+  const [showContactModal, setShowContactModal] = useState(true);
+
+  const [contactFormData, setContactFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const { user } = getAuthContext();
+
+  const { validateContactForm, validateMessageLength, contactErrors } =
+    useContactValidation();
+
+  useEffect(() => {
+    if (!user) {
+      setContactFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+    }
+
+    const fetchCurrentUser = async () => {
+      try {
+        const userDocRef = doc(database, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          setCurrentUserData(userDoc.data());
+        } else {
+          setCurrentUserData({});
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCurrentUser();
+  }, [user]);
+
+  useEffect(() => {
+    if (!currentUserData) {
+      return;
+    } else {
+      setContactFormData((prevData) => ({
+        ...prevData,
+        firstName: currentUserData.firstname || "",
+        lastName: currentUserData.lastname || "",
+        email: currentUserData.email || "",
+      }));
+    }
+  }, [currentUserData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContactFormData((prevData) => ({ ...prevData, [name]: value }));
+    if (name === "message") {
+      validateMessageLength(value, 400);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateContactForm(contactFormData)) {
+      console.log("Form Is not Valid");
+      return;
+    }
+    try {
+      const docRef = await addDoc(collection(database, "contactMessages"), {
+        ...contactFormData,
+        submittedAt: serverTimestamp(),
+      });
+      setShowContactModal(true);
+      console.log("Document added with the id", docRef.id);
+      setContactFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.contact}>
+        <form className={styles.contactForm} onSubmit={handleSubmit} noValidate>
+          {user ? (
+            <section className={styles.userContactSection}>
+              <div>
+                <h1>Hello {currentUserData.userName}!</h1>
+                <p>Need to get in touch?</p>
+                <p>
+                  We will get back to you at your registered e-mail:{" "}
+                  {currentUserData.email}, as soon as we can!
+                </p>
+              </div>
+            </section>
+          ) : (
+            <section className={styles.unknownContactSection}>
+              <fieldset>
+                <legend>Please fill out your contact information</legend>
+                <div className={styles.contactFormGroup}>
+                  <label htmlFor="firstName">First name</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="contactFomrFirstName"
+                    value={contactFormData.firstName}
+                    onChange={handleChange}
+                  />
+                  <p>{contactErrors.firstName}</p>
+                </div>
+                <div className={styles.contactFormGroup}>
+                  <label htmlFor="lastName">Last name</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="contactFormLastName"
+                    value={contactFormData.lastName}
+                    onChange={handleChange}
+                  />
+                  <p>{contactErrors.lastName}</p>
+                </div>
+                <div className={styles.contactFormGroup}>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="contactFormEmail"
+                    value={contactFormData.email}
+                    onChange={handleChange}
+                  />
+                  <p>{contactErrors.email}</p>
+                </div>
+              </fieldset>
+            </section>
+          )}
+          <section className={styles.messageSection}>
+            <fieldset>
+              <legend>Your message:</legend>
+
+              <div className={styles.contactFormGroup}>
+                <label htmlFor="subject">Subject</label>
+                <input
+                  type="text"
+                  name="subject"
+                  id="contactFormSubject"
+                  value={contactFormData.subject}
+                  onChange={handleChange}
+                />
+                <p>{contactErrors.subject}</p>
+              </div>
+              <div className={styles.contactFormGroup}>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="contactFormMessage"
+                  value={contactFormData.message}
+                  cols={15}
+                  rows={8}
+                  onChange={handleChange}
+                  className={styles.contactFormMessage}
+                ></textarea>
+                <span>
+                  {contactFormData.message ? contactFormData.message.length : 0}
+                  /400
+                </span>
+                <p>{contactErrors.message}</p>
+              </div>
+            </fieldset>
+          </section>
+          <div className={styles.contactFormToolsContainer}>
+            <Button className={styles.contactFormSubmit}>Send</Button>
+          </div>
+        </form>
+      </div>
+      {showContactModal && (
+        <Modal>
+          <div className={styles.contactModalContent}>
+            <h2>Your message has been delivered</h2>
+            <p>
+              Thank you for reaching out! We’ve received your message and will
+              get back to you as soon as possible.
+            </p>
+            <p>
+              We appreciate your patience and look forward to assisting you.
+            </p>
+            <Button
+              className={styles.closeModalButton}
+              onClick={() => setShowContactModal(false)}
+            >
+              Close
+            </Button>
+          </div>
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default Contact;
